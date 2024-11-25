2098b990c3c4d807903e3a5a2b66beb117de4742
MilBe2007 <mileny.santos@estudante.ifms.edu.br>
2024-11-25 14:34:45 -0400

    25-11

--- cliente/src/pages/Registro.jsx
+++ cliente/src/pages/Registro.jsx
@@ -1,9 +1,22 @@
 import { useState } from "react";
 
 export default function Registrar() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
 
+  const registrar = async(event) => {
+    event.preventDefault();
+    await fetch("http://localhost:3000/usuarios", {
+      method: 'POST',
+      headers: { 'Content-type': 'App'}
+    })
+  }
 
   return (
-        
+        <main>
+          <form action="" onSubmit={registrar}>
+
+          </form>
+        </main>
   );
 }
